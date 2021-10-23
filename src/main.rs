00eac7e0a19899fc91423c8a8b667c288f3903d3
Austin Scola <austinscola@gmail.com>
2021-10-23 11:53:20 -0400

    Add basic directory displaying

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
+fn display_directory() {
+    if let Ok(entries) = fs::read_dir(".") {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let file_name = entry.file_name();
+                let entry_name = file_name.to_string_lossy();
+                println!("{}", entry_name);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    display_directory();
 }
