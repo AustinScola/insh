2c4643747f8913eae81ac650c7f54af5e964b182
Austin Scola <austinscola@gmail.com>
2021-10-24 20:12:09 -0400

    Add changing of directory up or down

--- src/insh.rs
+++ src/insh.rs
@@ -1,8 +1,11 @@
 extern crate termion;
 
 use std::convert::TryInto;
+use std::env::current_dir;
 use std::fs;
 use std::io::{stdin, stdout, Stdout, Write};
+use std::iter::FromIterator;
+use std::path::PathBuf;
 use termion::color;
 use termion::event::Key;
 use termion::input::TermRead;
@@ -12,23 +15,48 @@ use termion::screen::*;
 pub struct Insh {
     screen: termion::screen::AlternateScreen<RawTerminal<Stdout>>,
     terminal_size: (u16, u16),
-    offset: usize,
     selected: usize,
+    directory: Box<PathBuf>,
+    entries: Vec<fs::DirEntry>,
+    entry_offset: usize,
 }
 
 impl Insh {
-    pub fn new() -> Self {
+    pub fn new() -> Insh {
         let screen = AlternateScreen::from(stdout().into_raw_mode().unwrap());
         let terminal_size = termion::terminal_size().unwrap();
-        let offset = 0;
         let selected = 0;
+        let directory: Box<PathBuf> = Box::new(current_dir().unwrap());
+        let entries_iter = fs::read_dir(&*directory).unwrap();
+        let entries: Vec<fs::DirEntry> = Vec::from_iter(
+            entries_iter
+                .take(terminal_size.1.into())
+                .map(|entry| entry.unwrap()),
+        );
+        let entry_offset = 0;
 
         Insh {
             screen,
             terminal_size,
-            offset,
             selected,
+            directory,
+            entries,
+            entry_offset,
         }
+    }
+
+    fn get_entries(&mut self) -> Vec<fs::DirEntry> {
+        let mut entries_iter = fs::read_dir(self.directory.as_path()).unwrap();
+        for _ in 0..self.entry_offset {
+            entries_iter.next();
+        }
+
+        let entries = Vec::from_iter(
+            entries_iter
+                .take(self.terminal_size.1.into())
+                .map(|entry| entry.unwrap()),
+        );
+        return entries;
     }
 
     pub fn run(&mut self) {
@@ -43,18 +71,44 @@ impl Insh {
                     if self.selected < self.terminal_size.1 as usize - 1 {
                         self.selected += 1;
                     } else {
-                        self.offset += 1
+                        self.entry_offset += 1
                     }
+                    self.entries = self.get_entries();
                     self.display_directory();
                 }
                 Key::Char('k') => {
                     if self.selected == 0 {
-                        if self.offset > 0 {
-                            self.offset -= 1
+                        if self.entry_offset > 0 {
+                            self.entry_offset -= 1
                         }
                     } else {
                         self.selected -= 1
                     }
+                    self.entries = self.get_entries();
+                    self.display_directory();
+                }
+                Key::Char('l') => {
+                    if self.entries.len() > 0 {
+                        let selected_path: PathBuf = self.entries[self.selected].path();
+
+                        if selected_path.is_dir() {
+                            self.directory.push(selected_path);
+                            if !self.directory.exists() {
+                                self.directory.pop();
+                            } else {
+                                self.selected = 0;
+                                self.entry_offset = 0;
+                                self.entries = self.get_entries();
+                                self.display_directory();
+                            }
+                        }
+                    }
+                }
+                Key::Char('h') => {
+                    self.directory.pop();
+                    self.selected = 0;
+                    self.entry_offset = 0;
+                    self.entries = self.get_entries();
                     self.display_directory();
                 }
                 _ => {}
@@ -72,11 +126,15 @@ impl Insh {
         write!(self.screen, "{}", termion::cursor::Show).unwrap();
     }
 
-    fn move_cursor(&mut self, x: usize, y: usize) {
+    fn move_cursor(
+        screen: &mut termion::screen::AlternateScreen<RawTerminal<Stdout>>,
+        x: usize,
+        y: usize,
+    ) {
         write!(
-            self.screen,
+            screen,
             "{}",
-            termion::cursor::Goto(x.try_into().unwrap(), (y + 1).try_into().unwrap())
+            termion::cursor::Goto((x + 1).try_into().unwrap(), (y + 1).try_into().unwrap())
         )
         .unwrap()
     }
@@ -84,35 +142,26 @@ impl Insh {
     fn display_directory(&mut self) {
         write!(self.screen, "{}", termion::clear::All).unwrap();
 
-        if let Ok(mut entries) = fs::read_dir(".") {
-            for _ in 0..self.offset {
-                entries.next();
-            }
+        for (entry_number, entry) in self.entries.iter().enumerate() {
+            Insh::move_cursor(&mut self.screen, 0, entry_number.into());
 
-            for (entry, entry_number) in entries.zip(0..self.terminal_size.1) {
-                self.move_cursor(1, entry_number.into());
-
-                if let Ok(entry) = entry {
-                    let file_name = entry.file_name();
-                    let entry_name = file_name.to_string_lossy();
-                    if usize::from(entry_number) == self.selected {
-                        write!(
-                            self.screen,
-                            "{}{}{}{}{}",
-                            color::Bg(color::White),
-                            color::Fg(color::Black),
-                            entry_name,
-                            color::Bg(color::Reset),
-                            color::Fg(color::Reset)
-                        )
-                        .unwrap();
-                    } else {
-                        write!(self.screen, "{}", entry_name).unwrap();
-                    }
-                }
+            let file_name = entry.file_name();
+            let entry_name = file_name.to_string_lossy();
+            if usize::from(entry_number) == self.selected {
+                write!(
+                    self.screen,
+                    "{}{}{}{}{}",
+                    color::Bg(color::White),
+                    color::Fg(color::Black),
+                    entry_name,
+                    color::Bg(color::Reset),
+                    color::Fg(color::Reset)
+                )
+                .unwrap();
+            } else {
+                write!(self.screen, "{}", entry_name).unwrap();
             }
         }
-
         self.screen.flush().unwrap();
     }
 }
