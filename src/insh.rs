93335ad733e314cb65e54e12ef76dfb83ebe1b7b
Austin Scola <austinscola@gmail.com>
2021-10-30 12:07:22 -0400

    Move finder to another module

--- src/insh.rs
+++ src/insh.rs
@@ -1,5 +1,15 @@
-extern crate crossterm;
+use std::cmp::Ordering;
+use std::convert::TryInto;
+use std::env::current_dir;
+use std::fs;
+use std::io::{self, Stdout, Write};
+use std::iter::FromIterator;
+use std::path::PathBuf;
+use std::process::{Child, Command, Stdio};
 
+use crate::finder::Finder;
+
+extern crate crossterm;
 use crossterm::{
     cursor,
     event::{self, Event, KeyCode, KeyEvent},
@@ -7,16 +17,6 @@ use crossterm::{
     terminal::{self, ClearType},
     QueueableCommand,
 };
-use regex::Regex;
-use std::cmp::Ordering;
-use std::convert::TryInto;
-use std::env::current_dir;
-use std::fs;
-use std::io::{self, Stdout, Write};
-use std::iter::FromIterator;
-use std::mem::swap;
-use std::path::{Path, PathBuf};
-use std::process::{Child, Command, Stdio};
 
 pub struct Insh {
     stdout: Stdout,
@@ -46,71 +46,6 @@ enum PatternState {
     NotCompiled,
     BadRegex,
     GoodRegex,
-}
-
-struct Finder {
-    regex: Regex,
-    stack: Vec<Box<dyn Iterator<Item = fs::DirEntry>>>,
-    iterator: Box<dyn Iterator<Item = fs::DirEntry>>,
-}
-
-impl Finder {
-    fn new(directory: &Path, pattern: &str) -> Result<Self, regex::Error> {
-        let regex = Regex::new(pattern)?;
-        let stack = Vec::new();
-        let iterator = Finder::get_directory_iterator(directory, regex.clone());
-
-        Ok(Finder {
-            regex,
-            stack,
-            iterator,
-        })
-    }
-
-    fn get_directory_iterator(
-        directory: &Path,
-        regex: Regex,
-    ) -> Box<dyn Iterator<Item = fs::DirEntry>> {
-        let directory_entries = fs::read_dir(&*directory).unwrap();
-        let filtered_entries = directory_entries
-            .filter(move |entry| {
-                entry.as_ref().unwrap().path().is_dir()
-                    || regex.is_match(&entry.as_ref().unwrap().file_name().to_string_lossy())
-            })
-            .map(|entry| entry.unwrap());
-        Box::new(filtered_entries)
-    }
-}
-
-impl Iterator for Finder {
-    type Item = fs::DirEntry;
-
-    fn next(&mut self) -> Option<fs::DirEntry> {
-        let next_entry = self.iterator.next();
-        match next_entry {
-            Some(entry) => {
-                if entry.path().is_dir() {
-                    let mut iterator =
-                        Finder::get_directory_iterator(&entry.path(), self.regex.clone());
-                    swap(&mut iterator, &mut self.iterator);
-                    self.stack.push(Box::new(iterator));
-                    self.next()
-                } else {
-                    Some(entry)
-                }
-            }
-            None => {
-                let next_iterator = self.stack.pop();
-                match next_iterator {
-                    Some(iterator) => {
-                        self.iterator = Box::new(iterator);
-                        self.next()
-                    }
-                    None => None,
-                }
-            }
-        }
-    }
 }
 
 impl Insh {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod finder;
 mod insh;
 
 fn main() {
