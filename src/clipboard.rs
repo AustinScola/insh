7e1c9568de0fdfb1ef4802fb8294e6e1ed7cbc36
Austin Scola <austinscola@gmail.com>
2022-10-09 15:54:40 -0400

    Fix yanking
    Yanking didn't seem to be working on a Ubuntu 22.04 machine that I have.
    
    The solution (that I dislike) is that getting the contents after setting
    them seems to fix it?

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -14,7 +14,24 @@ impl Clipboard {
 
     /// Set the contents of the clipboard.
     pub fn copy(&mut self, contents: String) {
-        self.context.set_contents(contents).unwrap();
+        #[cfg(feature = "logging")]
+        log::debug!("Setting the clipboard conents to \"{}\"...", contents);
+
+        #[allow(clippy::redundant_clone)]
+        self.context.set_contents(contents.clone()).unwrap();
+
+        // NOTE(ascola): We shouldn't have to do this, but setting contents doesn't seem to work
+        // on my laptop running Ubuntu 22.04 without it?
+        // See https://github.com/alacritty/copypasta/issues/49
+        #[allow(unused_variables)]
+        let actual_contents: String = self.context.get_contents().unwrap();
+
+        #[cfg(feature = "logging")]
+        if actual_contents != contents {
+            log::warn!("Failed to set the clipboard contents.");
+        } else {
+            log::debug!("Successfully set the clipboard contents.");
+        }
     }
 
     #[allow(dead_code)]
